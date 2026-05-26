b0ea838d5fc411a7f7acd1527b4f4a36ad9390d0
Gerhard Ahrens <Gerhard.Ahrens@lifeprojects.de>
2026-05-26 14:32:06 +0200

    *

---
+++ SnippetManager/Core/XamlTileItem.cs
@@ -0,0 +1,12 @@
+﻿namespace SnippetManager.Core
+{
+    using System.Diagnostics;
+    using System.Windows.Media;
+
+    [DebuggerDisplay("Tite: {this.Title}")]
+    public class XamlTileItem
+    {
+        public string Title { get; set; }
+        public DrawingImage ImageContent { get; set; }
+    }
+}
--- SnippetManager/ViewUC/XamlGrafikUC.xaml.cs
+++ SnippetManager/ViewUC/XamlGrafikUC.xaml.cs
@@ -1,7 +1,13 @@
 ﻿namespace SnippetManager.View
 {
+    using System.Collections;
+    using System.Collections.ObjectModel;
+    using System.Text;
     using System.Windows;
     using System.Windows.Controls;
+    using System.Windows.Media;
+    using System.Windows.Markup;
+    using System.Xml;
 
     using SnippetManager.Core;
 
@@ -24,6 +30,12 @@
         public CommandBase GoBackCommand { get; private set; }
         public CommandBase HelpCommand { get; private set; }
 
+        public ObservableCollection<XamlTileItem> XamlItemSource
+        {
+            get => base.GetValue<ObservableCollection<XamlTileItem>>();
+            set => base.SetValue(value);
+        }
+
         #endregion Properties
 
         #region Windows Events
@@ -40,6 +52,55 @@
             {
                 await App.EventAgg.PublishAsync(new WindowsTitelEvent("XamlGrafik Übersicht"));
             }
+
+            XamlItemSource = new ObservableCollection<XamlTileItem>();
+
+            const string DICTIONARYNAME = "Resources\\Style\\XamlIcon.xaml";
+
+            ResourceDictionary resourcesDic = Application.Current.Resources.MergedDictionaries.Where(md => md.Source.OriginalString.EndsWith(DICTIONARYNAME, StringComparison.CurrentCulture)).FirstOrDefault();
+            List<string> valueList = GetResourceKeys(resourcesDic);
+            foreach (string key in valueList)
+            {
+                var value = resourcesDic.Cast<DictionaryEntry>().FirstOrDefault(f => f.Key.ToString().Equals(key, StringComparison.OrdinalIgnoreCase)).Value;
+                if (value is DrawingImage drawingImage)
+                {
+                    XamlItemSource.Add(new XamlTileItem() { Title = key, ImageContent = drawingImage });
+                    /*
+                    string aa = GetXamlSourceFromKey(resourcesDic, key);
+                    */
+                }
+            }
+        }
+
+        private static List<string> GetResourceKeys(ResourceDictionary dictionary)
+        {
+            return dictionary.Keys.OfType<string>().ToList();
+        }
+
+        private static string GetXamlSourceFromKey(ResourceDictionary dictionary, string key)
+        {
+            // 1. Objekt aus dem ResourceDictionary laden
+            if (dictionary.Contains(key))
+            {
+                object resource = dictionary[key];
+
+                // 2. Objekt in XAML-String konvertieren
+                XmlWriterSettings settings = new XmlWriterSettings
+                {
+                    Indent = true,
+                    OmitXmlDeclaration = true
+                };
+
+                StringBuilder sb = new StringBuilder();
+                using (XmlWriter writer = XmlWriter.Create(sb, settings))
+                {
+                    XamlWriter.Save(resource, writer);
+                }
+
+                return sb.ToString();
+            }
+
+            return $"Key '{key}' nicht gefunden.";
         }
         #endregion Windows Events
 
